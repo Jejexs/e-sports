14fd3c08710a7bf39ee39f036a2fc569021d1f7e
charlesremy <charles.remy@my-digital-school.org>
2024-01-30 11:41:03 +0100

    first commit

---
+++ src/EScoreMAUI/EScoreMAUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EScoreMAUI;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ src/EScoreMAUI/EScoreMAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EScoreMAUI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
